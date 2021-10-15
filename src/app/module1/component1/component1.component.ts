666458998f1ac5e551c5fdce4a37beadee2d02e2
Edson Macamo <edsonmacamo85@gmail.com>
2021-10-15 16:50:13 +0200

    Primeiro commit

---
+++ src/app/module1/component1/component1.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { Service1 } from '../service1.service';
+
+@Component({
+  selector: 'app-component1',
+  // providers:[Service1],
+  templateUrl: './component1.component.html',
+  styleUrls: ['./component1.component.css']
+})
+export class Component1Component implements OnInit {
+
+  num = 0;
+  constructor(private myService1:Service1) { }
+
+  ngOnInit(){
+    this.num = this.myService1.num;
+  }
+
+}
